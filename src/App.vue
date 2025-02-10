a293e30dea1cc6a4975756103f49d259806a53a4
Hongliang Li <woodyli82@gmail.com>
2025-02-10 07:38:29 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-spacer />
+      <!-- hide-details 隐藏hint，可以使得text input与
+       toolbar中的其他元素行对齐-->
+      <v-text-field
+        placeholder="Search"
+        density="compact"
+        variant="solo"
+        prepend-inner-icon="mdi-magnify"
+        rounded
+        hide-details 
+      />
+      <v-spacer />
+      <v-btn icon="mdi-home"></v-btn>
+    </v-toolbar>
+    <router-view />
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
