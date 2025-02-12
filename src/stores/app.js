7ac3462918c71a57bb250741e05875b1d51416e1
Hongliang Li <woodyli82@gmail.com>
2025-02-12 05:54:09 -0800

    增加pinia注释

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,5 +1,17 @@
 // Utilities
 import { defineStore } from "pinia";
+// defineStore is a function that returns a function.
+// defineStore takes two arguments:
+//  the name of the store, and an object with the store's state
+// and actions.
+// To use:
+//   import useXXXStore from ...
+//   const store = useXXXStore()
+//   store.funcName()
+//   store.stateName = ...
+// 关于设计：
+// 在useAppStore第一次被import的时候，会调用defineStore()并返回访问store的方法。
+// 每次调用useAppStore()都会返回app对应的store对象，因此defineStore()只会被调用一次。
 
 export const useAppStore = defineStore("app", {
   state: () => ({
