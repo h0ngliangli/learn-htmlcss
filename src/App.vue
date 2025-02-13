5fd2a8bc8682781ae840146b1d8248650c23b9f0
Hongliang Li <woodyli82@gmail.com>
2025-02-12 17:20:33 -0800

    将v-app-bar放在deffault.vue中，因为它是布局的与部分。

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
 <template>
   <v-app :theme="appStore.theme">
-    <v-app-bar>
-      <v-app-bar-nav-icon id="menu-activator" />
-      <v-toolbar-title>H5</v-toolbar-title>
-      <!-- <v-text-field
-        placeholder="search"
-        rounded
-        variant="solo"
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-      >
-      </v-text-field> -->
-      <v-spacer />
-      <v-btn icon="mdi-lightbulb" @click="appStore.funcToggleTheme"></v-btn>
-    </v-app-bar>
-    <v-dialog activator="#menu-activator" max-width="500px">
-      <v-card>
-        <v-card-title>Menu</v-card-title>
-        <v-card-text>
-          <v-list>
-            <v-list-item v-for="route in getPageRoutes()" :key="route.path">
-              route-
-            </v-list-item>
-          </v-list>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
     <router-view />
   </v-app>
 </template>
 
 <script setup>
 import { useAppStore } from "@/stores/app";
-import { getPageRoutes } from "@/router";
 const appStore = useAppStore();
-const listEntries = () => {
-  console.log(getPageRoutes());
-};
 </script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,22 @@
 <template>
+  <v-app-bar>
+    <v-app-bar-nav-icon id="menu-activator" />
+    <v-toolbar-title>H5</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon="mdi-lightbulb" @click="appStore.funcToggleTheme"></v-btn>
+  </v-app-bar>
+  <v-dialog activator="#menu-activator" max-width="500px">
+    <v-card>
+      <v-card-title>Menu</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item v-for="route in getPageRoutes()" :key="route.path">
+            route-
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
   <v-main>
     <router-view />
   </v-main>
@@ -7,5 +25,7 @@
 </template>
 
 <script setup>
-  //
+import { useAppStore } from "@/stores/app";
+import { getPageRoutes } from "@/router";
+const appStore = useAppStore();
 </script>
